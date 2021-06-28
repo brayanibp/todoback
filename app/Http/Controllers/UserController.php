7ed00601f5b0d57cae22eed972bf0ebc0179a1ca
Brayan Bernal <32963009+brayanibp@users.noreply.github.com>
2021-06-27 22:09:56 -0400

    Adding laravel sanctum to make api tokens user authentication

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function signUp(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string',
+        ]);
+
+        User::create($request->all());
+
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('auth_token');
+
+        $token = $tokenResult->plainTextToken;
+
+        return response()->json([
+            'message' => 'Successfully created user!',
+            'access_token' => $tokenResult->plainTextToken,
+            'token_type' => 'Bearer',
+        ], 201);
+    }
+
+    /**
+     * Inicio de sesión y creación de token
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+            'remember_me' => 'boolean'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('auth_token');
+
+        return response()->json([
+            'access_token' => $tokenResult->plainTextToken,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    /**
+     * Cierre de sesión (anular el token)
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Obtener el objeto User como json
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
